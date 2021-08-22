e84c5c1d8e0d450477009c2c8f06e2654ef7bd2f
Luiz Felipe <luizfelipedasilva678@gmail.com>
2021-08-21 23:21:01 -0300

    Update discount.ts

--- src/ocp/classes/discount.ts
+++ src/ocp/classes/discount.ts
@@ -1,16 +1,17 @@
 export abstract class Discount {
-  abstract calculate(value: number): number;
-}
+  protected discount = 0;
 
-export class FiftyPercentDiscount extends Discount {
-  private readonly discount = 0.5;
   calculate(price: number): number {
     return price - price * this.discount;
   }
 }
 
-export class NoDiscount extends Discount {
-  calculate(price: number): number {
-    return price;
-  }
+export class FiftyPercentDiscount extends Discount {
+  protected readonly discount = 0.5;
 }
+
+export class TenPercentDiscount extends Discount {
+  protected readonly discount = 0.1;
+}
+
+export class NoDiscount extends Discount {}
